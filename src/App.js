6d8ec914bda8ad454b1aad1a65fb98eafa06aed6
masum691 <masumhu691@gmail.com>
2021-11-22 00:46:08 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
