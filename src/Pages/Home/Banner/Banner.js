dfe741dc6e937ec62be5eec35f0ed2c36bff0f61
masum691 <masumhu691@gmail.com>
2022-01-07 11:24:37 +0600

    fixed left banner and more

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import programmer from '../../../images/me.png'
+// import programmer from '../../../images/programmer.png'
+import programmer from '../../../images/programmer.png'
 import Navbar from '../../Navbar/Navbar';
 import './Banner.css'
 
@@ -7,26 +8,26 @@ const Banner = () => {
 
     return (
         <div className="banner-container">
-            <div className="with-nav container mx-auto ">
-            <div class="align-items-center portfolio-banner">
-                <div className='text-center'>
-                    <img className='programmer-banner-img' src={programmer} alt="" />
-                    <h5 className='mt-3'>MD MASUM MIA</h5>
-                    {/* <button>WEB DEVELOPER</button> */}
-                    <span>Web Developer</span>
-                    <p>I love learning and sharing knowledge.</p>
-                    <div className="font-awesome">
+            <div className="with-nav">
+                <div class="align-items-center portfolio-banner">
+                    <div className='text-center'>
+                        <img className='programmer-banner-img' src={programmer} alt="" />
+                        <h5 className='mt-3'>MD MASUM MIA</h5>
+                        {/* <button>WEB DEVELOPER</button> */}
+                        <span>Web Developer</span>
+                        <p>I love learning and sharing knowledge.</p>
+                        <div className="font-awesome">
 
-                        <a href="https://github.com/masum691/" target="_blank" rel="noreferrer"><i class="fab fa-github github"></i></a>
-                        <a href="https://web.facebook.com/profile.php?id=100071095920767" target="_blank" rel="noreferrer"><i class="fab fa-facebook facebook"></i></a>
-                        <i class="fab fa-twitter twitter"></i>
-                        <a href="https://www.linkedin.com/in/md-masum-mia-579ab320a/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin linkedin"></i></a>
+                            <a href="https://github.com/masum691/" target="_blank" rel="noreferrer"><i class="fab fa-github github"></i></a>
+                            <a href="https://web.facebook.com/profile.php?id=100071095920767" target="_blank" rel="noreferrer"><i class="fab fa-facebook facebook"></i></a>
+                            <i class="fab fa-twitter twitter"></i>
+                            <a href="https://www.linkedin.com/in/md-masum-mia-579ab320a/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin linkedin"></i></a>
+                        </div>
+
+                        <button className="resume mt-4"><a href="https://drive.google.com/file/d/16lPpu_jvLJmq3EFJBbqdy8QEB0AbFOkZ/view?usp=sharing" target="_blank" rel="noreferrer">Download Resume</a></button>
                     </div>
 
-                    <button className="resume mt-4"><a href="https://drive.google.com/file/d/16lPpu_jvLJmq3EFJBbqdy8QEB0AbFOkZ/view?usp=sharing" target="_blank" rel="noreferrer">Download Resume</a></button>
                 </div>
-                
-            </div>
             </div>
         </div>
     );
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,6 +5,7 @@ import Banner from '../Banner/Banner';
 import Blog from '../Blog/Blog';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
+import LeftBanner from '../LeftBanner/LeftBanner';
 import Projects from '../Projects/Projects';
 import Services from '../Services/Services';
 import TechSkills from '../TechSkills/TechSkills';
@@ -12,16 +13,23 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className='home-page container' id="home">
-            <Navbar></Navbar>
-            <Banner></Banner>
-            <About></About>
-            <TechSkills></TechSkills>
-            <Services></Services>
-            <Projects></Projects>
-            <Blog></Blog>
-            <Contact></Contact>
-            <Footer></Footer>
+        <div className='row container-fluid mx-auto'>
+            <div className='col-sm-12 col-md-4 col-lg-3 left-banner'>
+                <LeftBanner></LeftBanner>
+            </div>
+            <div className='col-sm-12 col-md-8 col-lg-9'>
+                <div className='home-page container' id="home">
+                    <Navbar></Navbar>
+                    {/* <Banner></Banner> */}
+                    <About></About>
+                    <TechSkills></TechSkills>
+                    <Services></Services>
+                    <Projects></Projects>
+                    <Blog></Blog>
+                    <Contact></Contact>
+                    <Footer></Footer>
+                </div>
+            </div>
         </div>
     );
 };
